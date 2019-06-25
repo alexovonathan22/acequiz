23b13a4d11b2c1a249f88baf7d844144f1e1baae
Atibaka Ogheneovo Alex <avo.nathan@gmail.com>
2019-06-25 20:33:03 +0100

    crud operation comple

--- public/aceMain.js
+++ public/aceMain.js
@@ -23,13 +23,13 @@ $(document).ready(function(){
         success: function(data){            
            $.each(data, function(i, q){
             $questions.append(`
-              <br> <li> ${num}.  ${q.question} <i data-id=${q.id} class="one fas fa-street-view"></i> <i data-id = ${q.id} class="edit fas fa-edit"></i> <i data-id = ${q.id} class="remove fas fa-trash-alt"></i></li><br>
+              <br> <li> ${num}. (<strong>Question id: ${q.id}</strong>)  ${q.question} <a href=http://localhost:5000/viewone.html><i class="one fas fa-street-view"></i></a> <i data-id = ${q.id} class="edit fas fa-edit"></i> <i data-id = ${q.id} class="remove fas fa-trash-alt"></i></li><br>
             `)
             num++;
            })       
         }
     });
-
+    
     $('#one').on('keyup', function(e){
         let id = e.target.value;
 
@@ -103,32 +103,93 @@ $(document).ready(function(){
         })
     })
 
-    //put req
-    // $questions.on('click', function(e){
-    //     // window.location.href = 'http://localhost:5000/allques.html';
-    //     e.preventDefault();
-    //     let question = {
-    //         question: $ques.val(),
-    //         option1: $option1.val(),
-    //         option2: $option2.val(),
-    //         correct_answer: $correctAns.val()
-    //     }
-
+    // put req
+  
+// $editQ.on('click',function(e){
         
-    //     $.ajax({
-    //         type: 'PUT',
-    //         url: `${url}quiz/${id}`,
-    //         data: question,
-    //         success: function(newQues){
-                
-    //              window.location.href = 'http://localhost:5000/allques.html';
-    //         },
-    //         error: function(){
-    //             alert('error editing question')
-    //         }
-    //     });
-    // })  
+    $('#editId').on('keyup', function(e){
+        let id = e.target.value;
 
+        $.ajax({
+            url: url+'quiz/'+id,
+            success: function(q){
+               
+                    $('#editQ').html(`
+                    <li data-id=${id}>
+                        <p>
+                            <strong>Question: </strong>
+                            <span class="noedit question">${q.question}</span>
+                            <input class="edit question">
+                        </p>
+                        <p>
+                            <strong>option1: </strong>
+                            <span class="noedit option1">${q.option1}</span>
+                            <input class="edit option1">
+                        </p>
+                        <p>
+                            <strong>option2: </strong>
+                            <span class="noedit option2">${q.option2}</span>
+                            <input class="edit option2">
+                        </p>
+                        <p>
+                            <strooption1ng>Correct Option: </strong>
+                            <span class="noedit correct_answer">${q.correct_answer}</span>
+                            <input class="edit correct_answer">
+                        </p>
+                        <button class="editQues noedit">Edit Question</button>
+                        <button class="saveQ edit">Save Question</button>
+                        <button class="cancelEdit edit">Cancel</button>
+                     </li>
+                `)
+                
+            }
+
+        })
+        
+    })
+
+    //editQ
+    $('#editQ').delegate('.editQues', 'click', function(){
+        let $li = $(this).closest('li');
+        $li.find('input.question').val($li.find('span.question').html());
+        $li.find('input.option1').val($li.find('span.option1').html());
+        $li.find('input.option2').val($li.find('span.option2').html());
+        $li.find('input.correct_answer').val($li.find('span.correct_answer').html());
+        $li.addClass('edit');
+    })
+
+    //canceledit
+    $('#editQ').delegate('.cancelEdit', 'click', function(){
+         $(this).closest('li').removeClass('edit');
+    })
+    //save put req
+    $('#editQ').delegate('.saveQ', 'click', function(){
+        let $li = $(this).closest('li'); 
+        let update = {
+            question: $li.find('input.question').val(),
+            option1: $li.find('input.option1').val(),
+            option2: $li.find('input.option2').val(),
+            correct_answer: $li.find('input.correct_answer').val()
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: `${url}quiz/${$li.attr('data-id')}`,
+            data: update,
+            success: function(newUpdate){
+                $li.find('span.question').html(newUpdate.question);
+                $li.find('span.option1').html(newUpdate.option1);
+                $li.find('span.option2').html(newUpdate.option2);
+                $li.find('span.correct_answer').html(newUpdate.correct_answer);
+                $li.removeClass('edit');
+                // window.location.href = 'http://localhost:5000/allques.html';
+            },
+            error: function(){
+                alert('error updating question');
+            }
+        });
+
+   })
 
     //answering the test
     
@@ -140,9 +201,9 @@ $(document).ready(function(){
             
            $.each(data, function(i, newQues){
 
-            let $li0 = `<input class="option" type="radio" name="q${d}" value="${newQues.option1}"> ${newQues.option1}`;
-            let $li1 = `<input class="option" type="radio" name="q${d}" value="${newQues.option2}"> ${newQues.option2}`;
-            let $li2 = `<input class="option" type="radio" name="q${d}" value="${newQues.correct_answer}"> ${newQues.correct_answer}`;
+            let $li0 = `<input type="radio" name="q${d}" value="${newQues.option1}"> ${newQues.option1}`;
+            let $li1 = `<input type="radio" name="q${d}" value="${newQues.option2}"> ${newQues.option2}`;
+            let $li2 = `<input type="radio" name="q${d}" value="${newQues.correct_answer}"> ${newQues.correct_answer}`;
             bucket = [$li0, $li1];
             bucket.splice(Math.floor(Math.random() * 2), 0, $li2);
             
